00c06f74b10652643fdc17c3841f9983180e4b23
Aakriti Bajekal <aakritibajekal@Aakritis-MacBook-Pro.local>
2020-05-19 20:56:32 -0600

    check now

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-index.html
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello, Heroku!</title>
+	</head>
+	<body>
+		<h1>Hello, Heroku!</h1>
+		<p>
+			This is my first container-based hosted website!
+		</p>
+	</body>
+</html> 
